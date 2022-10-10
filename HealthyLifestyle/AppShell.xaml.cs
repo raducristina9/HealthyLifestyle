68af97fa62e46916bd6c9ee8c43a96e6e38feafd
crist <raducristina18@stud.ase.ro>
2022-10-10 12:38:06 +0300

    add template .net maui + update .gitignore

---
+++ HealthyLifestyle/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HealthyLifestyle
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HealthyLifestyle/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HealthyLifestyle
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
