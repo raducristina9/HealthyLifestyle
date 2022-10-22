aee0bf7811f544efc9fed9377c29474137ebed30
crist <raducristina18@stud.ase.ro>
2022-10-22 17:08:54 +0300

    add buttons

--- HealthyLifestyle/MainPage.xaml.cs
+++ HealthyLifestyle/MainPage.xaml.cs
@@ -6,5 +6,15 @@
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+             Navigation.PushAsync(new  CalculatorKcal());
+        }
+
+        private  void Button_Clicked_1(object sender, EventArgs e)
+        {
+             Navigation.PushAsync(new CategoriiAlimente());
+        }
     }
 }
